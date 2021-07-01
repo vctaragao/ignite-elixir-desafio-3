97a7159bdaf8fe2ac03f7f3cc57b61a9940e6415
Victor Moraes de Aragao <vct.aragao@gmail.com>
2021-07-01 17:55:39 -0300

    Tratar arquivo para trasnformar as linhas do CSV em listas utilizaveis

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,28 @@
 defmodule GenReport do
+  @moduledoc """
 
-  def build do
-    # TO DO
+  """
+  def build(filename) do
+    File.stream!(filename)
+    |> Enum.map(fn line -> parse_line(line) end)
+
+    # |> Enum.reduce(%{}, fn line, acc -> generate_report(line, acc) end)
   end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> convert_tail_into_integer()
+  end
+
+  defp convert_tail_into_integer([head | tail]) do
+    [head | Enum.map(tail, &String.to_integer/1)]
+  end
+
+  # defp generate_report(_line, _acc) do
+  #   %{
+  #     all_hours: %{}
+  #   }
+  # end
 end
